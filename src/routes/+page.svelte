977fbd78e900e1b1a0ab06e796e8ad896970b8fb
Tom Pillot <tom.pillot3+github@gmail.com>
2022-11-10 00:27:44 +0100

    feat: add chart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,92 @@
-<script>
+<script lang="ts">
+import { Chart, registerables } from 'chart.js';
+import 'chartjs-adapter-moment';
+import { onMount } from 'svelte';
+
+Chart.register(...registerables);
+
+let barChartElement: HTMLCanvasElement;
+
+onMount(() => {
+	new Chart(barChartElement, {
+		type: 'line',
+		data: {
+			datasets: [{
+				data: [{
+					x: '2021-11-06 23:39:30',
+					y: 50
+				}, {
+					x: '2021-11-07 01:00:28',
+					y: 60
+				}, {
+					x: '2021-11-07 09:00:28',
+					y: 20
+				}]
+        	}],
+		},
+		options: {
+			scales: {
+				x: {
+					type: 'time',
+				}
+			}
+    	}
+	})
+})
+
 let data = 0;
 let err = "";
 
 async function fetchData() {
-	const response = await fetch('/api/data');
+	try {
+		const response = await fetch('/api/data');
+		const jsonResponse = await response.json()
 
-	let resp = await response.json();
-	console.log(resp)
+		if (response.status != 200) {
+			err = jsonResponse.message ?? "else"
+
+			return
+		}
+
+		data = jsonResponse
+	} catch (error) {
+		const message = `Failed to fetch data from API: ${err}`;
+		console.log(message);
+
+		err = message;
+		
+		return
+	}
 }
 </script>
 
-Data: {data}
-Error: {err}
+<main class="main-container">
+	<h1>Home dashboard</h1>
 
-<button on:click={fetchData}>Fetch data</button>
+	<button on:click={fetchData}>Refresh</button>
+
+	<section>
+		<canvas bind:this={barChartElement} />
+	</section>
+
+	<p>
+		Data: {data}
+	</p>
+
+	<p>
+		Error: {err}
+	</p>
+</main>
+
+<style>
+	h1 {
+	  color: var(--colour-dark);
+	  font-size: 3rem;
+	}
+
+	.main-container {
+	  width: min(100% - 3rem, 48rem);
+	  margin: 4.5rem auto;
+	}
+</style>
+  
--- src/routes/api/data/+server.ts
+++ src/routes/api/data/+server.ts
@@ -1,83 +1,86 @@
 import { json, error } from '@sveltejs/kit';
-import moment from "moment";
-import type { RequestEvent, RequestHandler } from './$types'
-import type { InfluxDBPoint, InfluxDBRow } from '$lib/types'
+import moment from 'moment';
+import type { RequestEvent, RequestHandler } from './$types';
+import type { InfluxDBPoint, InfluxDBRow } from '$lib/types';
 import Papa from 'papaparse';
- 
+
 export const GET: RequestHandler = async ({ params }: RequestEvent) => {
-    return json(fetchData())
-}
+	const url = `${process.env.INFLUXDB_URL}`;
+	const org = `${process.env.INFLUXDB_ORG}`;
+	const token = `${process.env.INFLUXDB_USER}:${process.env.INFLUXDB_PASSWORD}`;
 
-const url = `${process.env.INFLUXDB_URL}`
-const org = `${process.env.INFLUXDB_ORG}`
-const token = `${process.env.INFLUXDB_USER}:${process.env.INFLUXDB_PASSWORD}`
+	const fluxQuery = `from(bucket:"db0") 
+        |> range(start: -1d) 
+        |> filter(fn: (r) => r._measurement == "temperature" or r._measurement == "humidity")
+        |> movingAverage(n: 60)`;
 
-const fluxQuery =
-    `from(bucket:"db0") 
-    |> range(start: -1d) 
-    |> filter(fn: (r) => r._measurement == "temperature" or r._measurement == "humidity")
-    |> movingAverage(n: 60)`
+	return await fetch(`${url}/api/v2/query?org=${org}`, {
+		body: fluxQuery,
+		headers: {
+			Accept: 'application/csv',
+			Authorization: `Token ${token}`,
+			'Content-Type': 'application/vnd.flux'
+		},
+		method: 'POST'
+	})
+		.then((response) => {
+			return response.text();
+		})
+		.then((csv) => {
+			let dataPoints: { [time: string]: InfluxDBPoint } = {};
 
-const fetchData = () :  {[time: string]: InfluxDBPoint} => {
-    return await fetch(`${url}/api/v2/query?org=${org}`, {
-        body: fluxQuery,
-        headers: {
-            Accept: "application/csv",
-            Authorization: `Token ${token}`,
-            "Content-Type": "application/vnd.flux",
-        },
-        method: "POST",
-    }).then((response) => {
-        return response.text()
-    }).then((csv) => {
-        let dataPoints: { [time: string]: InfluxDBPoint } = {};
+			Papa.parse<InfluxDBRow>(csv, {
+				complete: (results) => {
+					for (const row of results.data) {
+						if (!Array.isArray(row) || row.length !== 9) {
+							continue;
+						}
 
-        Papa.parse<InfluxDBRow>(csv, {
-            complete: (results) => {
-                for (const row of results.data) {
-                    if (!Array.isArray(row) || row.length !== 9) {
-                        continue;
-                    }
+						const value = row[6];
+						if (isNaN(Number(value))) {
+							continue;
+						}
 
-                    const value = row[6];
-                    if (isNaN(Number(value))) {
-                        continue;
-                    }
+						const time = moment(row[5]);
+						if (!time.isValid()) {
+							continue;
+						}
 
-                    const time = moment(row[5]);
-                    if (!time.isValid()) {
-                        continue;
-                    }
+						const formattedTime = time.format('HH:mm');
+						const measurement = row[8];
+						switch (measurement) {
+							case 'temperature':
+								dataPoints[formattedTime] = {
+									time: formattedTime,
+									temperature: value,
+									humidity: dataPoints[formattedTime]?.humidity ?? ''
+								};
+								break;
+							case 'humidity':
+								dataPoints[formattedTime] = {
+									time: formattedTime,
+									temperature: dataPoints[formattedTime]?.temperature ?? '',
+									humidity: value
+								};
+								break;
+						}
+					}
 
-                    const formattedTime = time.format('HH:mm')
-                    const measurement = row[8];
-                    switch (measurement) {
-                        case "temperature":
-                            dataPoints[formattedTime] = {
-                                time: formattedTime,
-                                temperature: value,
-                                humidity: dataPoints[formattedTime]?.humidity ?? "",
-                            }
-                            break;
-                        case "humidity":
-                            dataPoints[formattedTime] = {
-                                time: formattedTime,
-                                temperature: dataPoints[formattedTime]?.temperature ?? "",
-                                humidity: value,
-                            }
-                            break;
-                    }
-                }
+					// Remove data points where we don't have both the temperature and humidity.
+					for (const time in dataPoints) {
+						if (!dataPoints[time].temperature || !dataPoints[time].humidity) {
+							delete dataPoints[time];
+						}
+					}
+				}
+			});
 
-                // Remove data points where we don't have both the temperature and humidity.
-                for (const time in dataPoints) {
-                    if (!dataPoints[time].temperature || !dataPoints[time].humidity) {
-                        delete dataPoints[time];
-                    }
-                }
-            }
-        });
+			return json({ dataPoints });
+		})
+		.catch((err) => {
+			const message = `Failed to fetch data from InfluxDB: ${err}`;
+			console.log(message);
 
-        return dataPoints
-    })
-}
+			throw error(500, message);
+		});
+};
